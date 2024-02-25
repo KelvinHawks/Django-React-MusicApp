d14c323d872856a5d0c49ce263054b6dfcb9351d
kelvin <kevohkyme35@gmail.com>
2024-02-25 23:20:16 +0300

    Initialize project

---
+++ music_controler/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class createRoomPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>This is the createRoomPage</h1>;
+  }
+}
